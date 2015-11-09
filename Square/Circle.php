077099a047ac1370741a0fc45f75759651924724
Shumer596 <ghost12@mail.ru>
2015-11-09 21:24:39 +0200

    Square

--- Square/Circle.php
+++ Square/Circle.php
@@ -85,6 +85,47 @@ class Square extends Circle
   }
 }
 
+class Rectangle extends Square
+{
+
+  /**
+   * @param int $param
+   * @param int $param2
+   * @throws Exception
+   */
+  public function setSquare($param, $param2)
+  {
+    if (is_int($param) || is_int($param2))
+    {
+      $this->_square = $param * $param2;
+    } else
+    {
+      throw new Exception('$param or $param2 are not integer type');
+    }
+  }
+
+
+  /**
+   * @param int $param
+   * @param int $param2
+   * @throws Exception
+   */
+  public function setPerimetr($param, $param2)
+  {
+    if (is_int($param) || is_int($param2))
+    {
+      $this->_perimetr = ($param + $param2) * 2;
+    } else
+    {
+      throw new Exception('$param or $param2 are not integer type');
+    }
+
+
+  }
+
+}
+
+
 $b = new Circle;
 $b->setSquare(5);
 $b->setPerimetr(5);
@@ -95,5 +136,9 @@ $a->setSquare(5);
 $a->setPerimetr(5);
 var_dump($a->getSquare());
 var_dump($a->getPerimetr());
-
+$c = new Rectangle;
+$c->setPerimetr(5,3);
+$c->setSquare(5,3);
+var_dump($c->getPerimetr());
+var_dump($c->getSquare());
 ?>
